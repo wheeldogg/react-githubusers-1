62ae252a10804e30e8f87ec497c93c915ffd5773
Shane Whelan <whelan.shane@nuritas.com>
2020-08-10 12:05:33 +0100

    added github handle link

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
 const Card = props => {
+  
   return (
     <div style={{ margin: '1em' }}>
       <img alt="avatar" style={{ width: '70px' }} src={props.avatar_url} />
       <div>
         <div style={{ fontWeight: 'bold' }}>{props.name}</div>
         <div>{props.blog}</div>
+        <a href={props.html_url}>Github handle</a>
       </div>
     </div>
+    
   )
 }
 
